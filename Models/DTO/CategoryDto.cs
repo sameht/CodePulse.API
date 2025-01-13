09c33677b30b206d73cd61860a5ba6a1630f87af
ali-tit <ali.benali@tunisianit.com>
2025-01-13 16:28:47 +0100

    first commit

---
+++ Models/DTO/CategoryDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodePulse.API.Models.DTO;
+
+public class CategoryDto
+{
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string UrlHandle { get; set; }
+}
---
+++ Models/Domain/BlogPost.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CodePulse.API.Models.Domain;
+
+public class BlogPost
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string shortDescription { get; set; }
+    public string Content { get; set; }
+    public string FeaturedImageUrl { get; set; }
+    public string UrlHandle { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public string Author { get; set; }
+    public bool IsVisible { get; set; }
+}
